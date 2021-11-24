7ba8b55edd63ac8fc8cbce0629b784b5610a6b2f
Romar1 <35073934+Romar159@users.noreply.github.com>
2021-11-24 20:05:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     TOKEN: process.env.token,
     PREFIX: "p<",
-    VERSION: "0.5.0dep",
+    VERSION: "0.5.0-InDev",
     DBCONNECTION: "mongodb+srv://" + process.env.dbuser + ":" + process.env.dbpass + "@cluster0.gw6s2.mongodb.net/PouleRPG?retryWrites=true&w=majority",
     DEFAULTSETTINGS: {
         prefix: "p<"
